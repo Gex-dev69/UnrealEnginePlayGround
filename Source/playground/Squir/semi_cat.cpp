39e48a1d832c7b74bc179e0ab592f905662e7d7a
Gex-dev69 <dgexion@outlook.com>
2023-03-06 23:40:12 +0300

    Created Custom mode

--- Source/playground/Squir/semi_cat.cpp
+++ Source/playground/Squir/semi_cat.cpp
@@ -37,7 +37,7 @@ Asemi_cat::Asemi_cat()
 	// instead of recompiling to adjust them
 	GetCharacterMovement()->JumpZVelocity = 700.f;
 	GetCharacterMovement()->AirControl = 0.35f;
-	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MaxWalkSpeed = 200.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 
